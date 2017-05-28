3fa516f6f6bf1433810c049e5bc15e4425a49db1
Dekatron <onigbindeayomide@gmail.com>
2017-05-28 16:55:28 +0100

    akajdad

--- index.js
+++ index.js
@@ -189,7 +189,6 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
 }
 
 // function receivedPostback(event) {
