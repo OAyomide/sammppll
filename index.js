c96f163745bdde7db3fd4d4d22c15c2008eb1e38
Dekatron <onigbindeayomide@gmail.com>
2017-07-11 04:40:23 +0100

    ajdhajkad

--- index.js
+++ index.js
@@ -709,7 +709,7 @@ case 'davido_select':
   break;
 case 'buy_car':
 new Promise(function(resolve, reject) {
-}).then(carBought(senderID)).then(sendTextMessage(senderID, "Nice"))
+}).then(party(senderID)).then(sendTextMessage(senderID, "Nice"))
   break;
   case 'party_hard':
     party(senderID);
