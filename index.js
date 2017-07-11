b6c6cb7c7744e50ff3276b511cbeffa64cb1f5e4
Dekatron <onigbindeayomide@gmail.com>
2017-07-11 03:53:56 +0100

    adjad

--- index.js
+++ index.js
@@ -710,7 +710,7 @@ case 'davido_select':
 case 'buy_car':
 
 new Promise(function(resolve, reject) {
-}).then(setTimeout(carBought(senderID), 3000)).then(eventually = promiseDelay(5000, Promise.resolve(sendTextMessage(senderID,"NiceRide"))))
+}).then(setTimeout(carBought(senderID), 500)).then(eventually = promiseDelay(5000, Promise.resolve(sendTextMessage(senderID,"NiceRide"))))
   break;
   case 'party_hard':
     party(senderID);
