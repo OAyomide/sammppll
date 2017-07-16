1ae32cacf6bf55aaeb5ddc9d46228acc4d67c197
Dekatron <onigbindeayomide@gmail.com>
2017-07-16 19:22:45 +0100

    ajdhajd

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var request = require('request');
 var promiseDelay = require('promise-delay');
 var randomFuncts = require('./models/funcs')
 var coreFunct = require('./models/corefunct');
+var stuff = require('./models/stuffs');
 var recastai = require('recastai');
 var cars = [];
 
@@ -102,7 +103,7 @@ request.analyseText(messageText).then(function(res){
 if (messageText){
   switch (messageText) {
     case 'menu':
-    quickButtons(senderID);
+    stuff.quickButtons(senderID);
       break;
     case 'celebs':
       init(senderID);
@@ -774,64 +775,8 @@ break;
 // }
 
 
-function quickButtons(recipientId){
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message:{
-    text:"Quick action",
-    quick_replies:[
-      {
-        content_type:"text",
-        title:"Announcement",
-        payload:"emergency"
-      },
-      {
-        content_type:"text",
-        title:"Download latest sermon",
-        payload:"download"
-      },
-      {
-        content_type:"text",
-        title:"New sermon",
-        payload:"sermon"
-      }
-    ]
-  }
-}
-callSendAPI(messageData);
-}
 
 
-
-
-
-
-// function callSendAPI (messageData){
-//   request({
-//     uri: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: {
-//       access_token: fb_page_token
-//     },
-//     method: 'POST',
-//     json: messageData
-//   }, function(error, response, body){
-//     if (!error && response.statusCode == 200) {
-//       var recipientId = body.recipient_id;
-//       var messageId = body.message_id;
-//       console.log('Successfully sent generic message with id %s to recipient %s', messageId, recipientId);
-//     }
-//     else {
-//       console.error('Unable to send message.');
-//       console.error(response);
-//       console.error(error);
-//     }
-//   });
-// }
-
-//we want to subscribe the user to receive the notification. For user ids that exist in the db, send them subscriptions
-//read request npm
 
 app.listen(port, function(req, res){
   console.log('Bot app up and running on port: ', port);
---
+++ models/stuffs.js
@@ -0,0 +1,52 @@
+exports.quickButtons = function quickButtons(recipientId){
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message:{
+    text:"Quick action",
+    quick_replies:[
+      {
+        content_type:"text",
+        title:"Announcement",
+        payload:"emergency"
+      },
+      {
+        content_type:"text",
+        title:"Download latest sermon",
+        payload:"download"
+      },
+      {
+        content_type:"text",
+        title:"New sermon",
+        payload:"sermon"
+      }
+    ]
+  }
+}
+callSendAPI(messageData);
+}
+
+
+
+function callSendAPI (messageData){
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: fb_page_token
+    },
+    method: 'POST',
+    json: messageData
+  }, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+      console.log('Successfully sent generic message with id %s to recipient %s', messageId, recipientId);
+    }
+    else {
+      console.error('Unable to send message.');
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
